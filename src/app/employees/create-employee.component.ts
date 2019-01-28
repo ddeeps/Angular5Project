2473a33c9fa36a10b7a8ea815e9a6ed359d2ce12
drani <deepika.rani@yestelecom.nl>
2019-01-28 12:31:58 +0530

    changes

--- src/app/employees/create-employee.component.ts
+++ src/app/employees/create-employee.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgForm } from '@angular/forms';
+import { department } from '../models/department.model';
+import { BsDatepickerConfig } from 'ngx-bootstrap/datepicker';
 
 @Component({
   selector: 'app-create-employee',
@@ -7,15 +9,39 @@ import { NgForm } from '@angular/forms';
   styleUrls: ['./create-employee.component.css']
 })
 export class CreateEmployeeComponent implements OnInit {
-  department = "3";
-  
-  constructor() { }
+  previewPhoto = false;
+  display = "Show";
+  dateOfBirth = '12/01/2018'; 
+  datePickerConfig: Partial<BsDatepickerConfig>;
+  departments: department[] = [
+    { id: 1, name: 'Help Desk' },
+    { id: 2, name: 'HR' },
+    { id: 3, name: 'IT' },
+    { id: 4, name: 'Payroll' },
+    { id: 5, name: 'Admin' }
+  ];
+
+  constructor() {
+    this.datePickerConfig = Object.assign({}, 
+      { containerClass: 'theme-dark-blue',
+       showWeekNumbers: false,
+       minDate : new Date(2018, 0, 1),
+       maxDate : new Date(2018, 11, 31),
+       dateInputFormat : 'DD/MM/YYYY'
+       }
+    );
+  }
 
   ngOnInit() {
   }
 
   SaveEmployee(empForm: NgForm): void {
     console.log(empForm);
+  }
+
+  togglePhoto(){
+    this.previewPhoto = !this.previewPhoto;
+    this.display = this.display == "Show"? "Hide":"Show";
   }
 
 }
